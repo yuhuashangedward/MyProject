a9ee0edf538c0f26d42f12aa63893563cb1603b4
yuhuashangedward <yuhuashang@aliyun.com>
2018-01-22 10:57:41 +0800

    更新测试文件

--- src/com/alibaba/demo/GitTest.java
+++ src/com/alibaba/demo/GitTest.java
@@ -3,5 +3,6 @@ package com.alibaba.demo;
 public class GitTest {
     public static void main(String[] args){
         System.err.println("First Demo for git!");
+        System.err.println("Every Test for Git!");
     }
 }
