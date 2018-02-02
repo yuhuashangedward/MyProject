7fdd48c5e4a6cbcfd3b0c2d22d482c29a7815c14
yuhuashangedward <yuhuashang@aliyun.com>
2018-02-02 20:21:41 +0800

    银行卡识别测试

---
+++ src/com/alibaba/demo/BankTest.java
@@ -0,0 +1,12 @@
+/*
+package com.alibaba.demo;
+
+import com.alibaba.demo.entity.AssetsBankInfo;
+
+public class BankTest {
+    public static void main(String[] args){
+        System.err.println("Hello ");
+        AssetsBankInfo.getNameOfBank();
+    }
+}
+*/
---
+++ src/com/alibaba/demo/NumberTest.java
@@ -0,0 +1,8 @@
+package com.alibaba.demo;
+
+public class NumberTest {
+    public static void main(String[] args){
+        int a = 1;
+        System.err.println((byte)1);
+    }
+}
