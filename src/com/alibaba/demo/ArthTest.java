d24900a1732b6a335324c61115804dae72543c5c
yuhuashangedward <yuhuashang@aliyun.com>
2018-02-07 20:47:44 +0800

    三目运算符测试

---
+++ src/com/alibaba/demo/ArthTest.java
@@ -0,0 +1,12 @@
+package com.alibaba.demo;
+
+public class ArthTest {
+
+    public static void main(String[] args){
+        System.err.println("hello");
+
+        boolean b = 8>5 ? true:false;
+        System.err.println(b);
+    }
+
+}
