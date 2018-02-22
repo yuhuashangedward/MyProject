a4338e3c181395624f798bde8afccafea1a5ab76
yuhuashangedward <yuhuashang@aliyun.com>
2018-02-22 08:56:40 +0800

    更新

---
+++ src/com/alibaba/demo/DemoTest.java
@@ -0,0 +1,10 @@
+package com.alibaba.demo;
+
+public class DemoTest {
+
+    public static void main(String[] args){
+        System.err.println(
+                "Hello 2018!"
+        );
+    }
+}
