115f69099a1805c042047b5b7f430f1be86031da
yuhuashangedward <yuhuashang@aliyun.com>
2018-02-24 13:07:28 +0800

    毫秒与时间的转化

--- src/com/alibaba/demo/utils/TimeUtil.java
+++ src/com/alibaba/demo/utils/TimeUtil.java
@@ -7,7 +7,8 @@ public class TimeUtil {
 
     public static void main(String[] args){
 
-        long time = 1519389126076L;
+        //long time = 1519389126076L;
+        long time = 1519443311620L;
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 
