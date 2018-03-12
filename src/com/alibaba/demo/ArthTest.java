45e974da87a14de4524782e18b72e9acb471e5c8
yuhuashangedward <yuhuashang@aliyun.com>
2018-03-12 14:43:51 +0800

    BigDecimal用法测试

--- src/com/alibaba/demo/ArthTest.java
+++ src/com/alibaba/demo/ArthTest.java
@@ -1,11 +1,17 @@
 package com.alibaba.demo;
 
+import java.math.BigDecimal;
+
 public class ArthTest {
 
     public static void main(String[] args){
         System.err.println("hello");
 
         boolean b = 8>5 ? true:false;
+
+        System.out.println(new BigDecimal("20.003450").stripTrailingZeros());
+        System.out.println(new BigDecimal("20.003450").stripTrailingZeros().toString());
+        System.out.println(new BigDecimal("20.003450").stripTrailingZeros().toPlainString());
         System.err.println(b);
     }
 
