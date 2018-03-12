6aa581b01323f82ef7b3fca27d2426c0a8286dcc
yuhuashangedward <yuhuashang@aliyun.com>
2018-03-12 15:25:16 +0800

    BigDecimal用法测试

--- src/com/alibaba/demo/ArthTest.java
+++ src/com/alibaba/demo/ArthTest.java
@@ -9,10 +9,17 @@ public class ArthTest {
 
         boolean b = 8>5 ? true:false;
 
-        System.out.println(new BigDecimal("20.003450").stripTrailingZeros());
+       /* System.out.println(new BigDecimal("20.003450").stripTrailingZeros());
         System.out.println(new BigDecimal("20.003450").stripTrailingZeros().toString());
         System.out.println(new BigDecimal("20.003450").stripTrailingZeros().toPlainString());
-        System.err.println(b);
+        System.out.println(BigDecimal.ZERO);
+        System.err.println(b);*/
+
+        System.err.println("++++++++++++++++++++++++++++++++++++====================================++++++++++++++++++++++++++");
+        System.out.println(new BigDecimal("20.003450").setScale(3, BigDecimal.ROUND_HALF_DOWN));
+        System.out.println(new BigDecimal("20.003450").setScale(4, BigDecimal.ROUND_HALF_UP));
+        System.out.println(new BigDecimal("20.003450").setScale(4, BigDecimal.ROUND_HALF_EVEN));
+        System.out.println(new BigDecimal("20.003450").setScale(3,BigDecimal.ROUND_CEILING));
     }
 
 }
